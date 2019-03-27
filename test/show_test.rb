bf28c0eb73e310444d4481f7402d63c5299a229c
Patrick Duvall <duvallp208@gmail.com>
2019-03-27 09:16:02 -0600

    Iteration 2

---
+++ test/show_test.rb
@@ -0,0 +1,24 @@
+require "./lib/charachter"
+require "./lib/show"
+require "minitest/autorun"
+
+class ShowTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_has_attributes
+    assert_equal "Knight Rider", @knight_rider.name
+    assert_equal "Glen Larson", @knight_rider.creator
+    assert_equal [@michael_knight, @kitt], @knight_rider.characters
+  end
+
+  def test_total_salaries_of_charachters
+    assert_equal 2600000, @knight_rider.total_salary
+  end
+
+
+end
